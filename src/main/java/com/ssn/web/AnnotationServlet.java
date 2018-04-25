c5f8be1d6a0b4cfca2e6478d30b61a5524aad9a6
Ahamed <syed.ahamed@hp.com>
2018-04-25 19:24:41 +0530

    submitting form

--- src/main/java/com/ssn/web/AnnotationServlet.java
+++ src/main/java/com/ssn/web/AnnotationServlet.java
@@ -1,28 +1,38 @@
 package com.ssn.web;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/servlettwo")
+
+@WebServlet(
+        urlPatterns = "/servlettwo",
+        initParams =
+        {
+            @WebInitParam(name = "saveDir", value = "D:/FileUpload"),
+            @WebInitParam(name = "allowedTypes", value = "jpg,jpeg,gif,png")
+        }
+)
+
+
+
 public class AnnotationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
- 
-    public AnnotationServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	
+	protected void doGet(HttpServletRequest request, 
+						HttpServletResponse response) 
+								throws ServletException, IOException {
 		response.getWriter()
 				.append("<h1>Hellow from second servlet</h1> ")
 				.append("<h1>"+getServletContext().getInitParameter("AppName")+"</h1>")
+				.append("<h1>"+getInitParameter("saveDir")+"</h1>")
+				
 				.append(request.getContextPath());
 	}
 
--- src/main/java/com/ssn/web/Home.java
+++ src/main/java/com/ssn/web/Home.java
@@ -2,6 +2,7 @@ package com.ssn.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Enumeration;
 
 import javax.servlet.ServletConfig;
@@ -26,22 +27,26 @@ public class Home extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
 		// set header
 		resp.setContentType("text/html");
 		
 		//conext params
 	    PrintWriter printWriter  = resp.getWriter();
-	    
-	   
-	    
+
 	    printWriter.println("<h1>"+getServletContext().getInitParameter("AppName")+"</h1>");
 	    printWriter.println("<h2>"+getServletContext().getInitParameter("description")+"</h2>");
 	    printWriter.println("<h2>"+getInitParameter("Greetings")+"</h2>");
 	    //init params
 	    
+	    String param1 = req.getParameter("param1");
+		String param2 = req.getParameter("param2");
+		String[] paramArray = req.getParameterValues("paramArray");
+		
+		printWriter.println("<p>param1:"+param1+"</p>");
+		printWriter.println("<p>param2:"+param2+"</p>");
+		printWriter.println("<p>paramArray:"+Arrays.toString(paramArray)+"</p1>");
 
-	    
+	        
 	}
 
     
